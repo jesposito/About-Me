f4e7082ea9b798cd91978d664c4fb4081986f960
Jedidiah Esposito <egerthe@gmail.com>
2017-05-25 20:59:18 -0700

    First question implemented

--- app.js
+++ app.js
@@ -6,18 +6,30 @@ var confirmed = confirm('Welcome! Before you go any further, I\'d like to find o
 if(confirmed === true){
   console.log('The user is ready!');
   //let the user know we heard them.
-  alert('Great! Get ready for the first question.');
+  alert('Great! I\'ll call the Quiz Master over.');
   } else {
     //let the user know we can't stop it now.
     console.log('The user said they were not ready... but we\'re gonna start anyway.');
-    alert('Ah nuts. I already sen\'t for the quiz master. He\'ll be here any minute. Be a doll and do it anyway, will ya?');
+    alert('Ah nuts. I already sen\'t for the Quiz Master. He\'ll be here any minute. Be a pal and do it anyway, will ya?');
 }
 //introduce the quiz maaster who will be asking the questions.
 alert('You called? Who needs quizzing?');
 //razz the user a little for fun.
 alert('Oh... it\'s this one. (sigh) alright then. Let\'s get it over with.');
 
-prompt('Question 1: Has Jed ever eaten a live squid?');
+//declare and ask the first question.
+var a1 = prompt('Question 1: Has Jed ever eaten a live squid?');
+//determine whether the user answered yes or no.
+if(a1.toUpperCase === 'Y'||'YES') {
+  var a1Result = 'correctly';
+  console.log('Hm... this person might know Jed.');
+  alert('MmHmm, MmHmm, very interesting.');
+  } else {
+    var a1Result = 'incorrectly';
+    console.log('Strike 1!');
+    alert('MmHmm, MmHmm, very interesting.');
+}
 
 
-document.querySelector('.answers').innerHTML = '<p>Answer 1: ' + q1 + '<br>Answer 2: ' + q2+ '</p>'
+//Write the user's answers to the page as they answer
+document.querySelector('.answers').innerHTML = '<p>The Quiz Master asked you whether Jed has ever eaten a live squid. You ' + a1Result + ' said ' +'\"'+a1+'\".'+'</p>'
