7f0ace09ad6af0f202eb5df9dafcbd2636f293d6
Jedidiah Esposito <egerthe@gmail.com>
2017-05-25 21:29:43 -0700

    refactored the way answers display on the page to account for multiple answers

--- app.js
+++ app.js
@@ -26,16 +26,16 @@ if(a1.toUpperCase === 'Y'||'YES') {
   alert('MmHmm, MmHmm, very interesting.');
   } else {
     var a1Result = 'incorrectly';
-    console.log('Strike 1!');
+    console.log('Strike!');
     alert('MmHmm, MmHmm, very interesting.');
 }
 
-//declare and ask the first question.
-var a2 = prompt('Question 1: Has Jed ever eaten a live squid?');
+//declare and ask the second question.
+var a2 = prompt('Question 2: Has Jed ever been to Paris?');
 //determine whether the user answered yes or no.
-if(a1.toUpperCase === 'Y'||'YES') {
+if(a2.toUpperCase === 'N'||'NO') {
   var a1Result = 'correctly';
-  console.log('Hm... this person might know Jed.');
+  console.log('Not yet, anyway.');
   alert('MmHmm, MmHmm, very interesting.');
   } else {
     var a2Result = 'incorrectly';
@@ -43,12 +43,12 @@ if(a1.toUpperCase === 'Y'||'YES') {
     alert('MmHmm, MmHmm, very interesting.');
 }
 
-//declare and ask the first question.
-var a3 = prompt('Question 1: Has Jed ever eaten a live squid?');
+//declare and ask the third question.
+var a3 = prompt('Question 3: If Jed were an animal, would he be a Polar Bear?');
 //determine whether the user answered yes or no.
-if(a1.toUpperCase === 'Y'||'YES') {
+if(a3.toUpperCase === 'Y'||'YES') {
   var a1Result = 'correctly';
-  console.log('Hm... this person might know Jed.');
+  console.log('YES, BECAUSE THEY ARE THE BEST.');
   alert('MmHmm, MmHmm, very interesting.');
   } else {
     var a3Result = 'incorrectly';
@@ -56,25 +56,25 @@ if(a1.toUpperCase === 'Y'||'YES') {
     alert('MmHmm, MmHmm, very interesting.');
 }
 
-//declare and ask the first question.
-var a4 = prompt('Question 1: Has Jed ever eaten a live squid?');
+//declare and ask the fourth question.
+var a4 = prompt('Question 4: When Jed shares his profession, do people usually look at him quizically?');
 //determine whether the user answered yes or no.
-if(a1.toUpperCase === 'Y'||'YES') {
+if(a4.toUpperCase === 'Y'||'YES') {
   var a4Result = 'correctly';
-  console.log('Hm... this person might know Jed.');
+  console.log('What\'s an Instructional Designer?');
   alert('MmHmm, MmHmm, very interesting.');
   } else {
-    var a2Result = 'incorrectly';
+    var a4Result = 'incorrectly';
     console.log('Strike!');
     alert('MmHmm, MmHmm, very interesting.');
 }
 
-//declare and ask the first question.
-var a5 = prompt('Question 1: Has Jed ever eaten a live squid?');
+//declare and ask the fifth question.
+var a5 = prompt('Question 5: Do you like Polar Bears, too?');
 //determine whether the user answered yes or no.
-if(a1.toUpperCase === 'Y'||'YES') {
-  var a1Result = 'correctly';
-  console.log('Hm... this person might know Jed.');
+if(a5.toUpperCase === 'Y'||'YES') {
+  var a5Result = 'correctly';
+  console.log('HECK YES YOU DO, BECAUSE THEY\'RE FREAKING AWESOME!');
   alert('MmHmm, MmHmm, very interesting.');
   } else {
     var a5Result = 'incorrectly';
@@ -83,4 +83,8 @@ if(a1.toUpperCase === 'Y'||'YES') {
 }
 
 //Write the user's answers to the page as they answer
-document.querySelector('.answers').innerHTML = '<p>The Quiz Master asked you whether Jed has ever eaten a live squid. You ' + a1Result + ' said ' +'\"'+a1+'\".'+'</p><br>'
+document.querySelector('.answer1').innerHTML = '<p>The Quiz Master asked you whether Jed has ever eaten a live squid. You ' + a1Result + ' said ' +'\"'+a1+'\".'+'</p>'
+document.querySelector('.answer2').innerHTML = '<p>He then asked whether Jed has been to paris or not. You ' + a2Result + ' said ' +'\"'+a2+'\".'+'</p>'
+document.querySelector('.answer3').innerHTML = '<p>This next one was strange. How are you supposed to know whether or not Jed would choose to be a Polar Bear?. You ' + a3Result + ' said ' +'\"'+a3+'\".'+'</p>'
+document.querySelector('.answer4').innerHTML = '<p>Next, he asked whether people knew generally knew what an Instructional Designer was. You ' + a4Result + ' said ' +'\"'+a4+'\".'+'</p>'
+document.querySelector('.answer5').innerHTML = '<p>Thank goodness this was the last question. That quizmaster has terrible breath. He brought up the Polar Bear again, asking whether or not you like them. You ' + a5Result + ' said ' +'\"'+a5+'\".'+'</p>'
