0669a48ce62ae2c45ef8adb6ab9d0d2ff625868f
Jedidiah Esposito <egerthe@gmail.com>
2017-05-26 18:05:17 -0700

    formatted if-else statements

--- app.js
+++ app.js
@@ -20,25 +20,26 @@ alert('Oh... it\'s this one. (sigh) alright then. Let\'s get it over with.');
 //declare and ask the first question.
 var q1 = prompt('Question 1: Has Jed ever eaten a live squid?');
 q1 = q1.toUpperCase();
+//log this for troubleshooting
 console.log(q1);
 //determine whether the user answered yes or no.
-if(q1 == 'Y'||q1 == 'YES') {
+if(q1 == 'Y'||q1 == 'YES') { //checking for either y or yes.
   console.log(q1);
   var a1 = 'Yes';
-  var a1Result = 'correctly';
+  var a1Result = ' which was correct';
   console.log('Hm... this person might know Jed.');
   alert('MmHmm, MmHmm, very interesting.');
   }
   else if (q1 == 'N'||q1=='NO') {
     var a1 = 'No';
-    var a1Result = 'incorrectly';
+    var a1Result = ' which was incorrect';
     console.log('Strike!');
     alert('MmHmm, MmHmm, very interesting.');
     }
     else {
       var a1 = q1;
-      var a1Result = 'something that made no sense.';
-    };
+      var a1Result =  ' which made no sense';
+      };
 
 //declare and ask the second question.
 var q2 = prompt('Question 2: Has Jed ever been to Paris?');
@@ -46,17 +47,19 @@ q2 = q2.toUpperCase();
 //determine whether the user answered yes or no.
 if(q2 == 'N'||q2 =='NO') {
   var a2 = 'No';
-  var a2Result = 'correctly';
+  var a2Result = ' which was correct';
   console.log('Not yet, anyway.');
   alert('MmHmm, MmHmm, very interesting.');
-} else  if (q2 == 'Y'||q2=='YES') {
-      var a2 = 'Yes';
-      var a2Result = 'incorrectly';
-      console.log('Strike!');
-      alert('MmHmm, MmHmm, very interesting.');
-  }    else {
-        var a2 = q2;
-        var a2Result = 'something that made no sense.';
+  }
+  else  if (q2 == 'Y'||q2=='YES') {
+    var a2 = 'Yes';
+    var a2Result = ' which was incorrect';
+    console.log('Strike!');
+    alert('MmHmm, MmHmm, very interesting.');
+    }
+    else {
+      var a2 = q2;
+      var a2Result = ' which made no sense';
       };
 
 //declare and ask the third question.
@@ -65,17 +68,19 @@ q3 = q3.toUpperCase();
 //determine whether the user answered yes or no.
 if(q3 == 'Y'||q3=='YES') {
   var a3 = 'Yes';
-  var a3Result = 'correctly';
+  var a3Result = ' which was correct';
   console.log('YES, BECAUSE THEY ARE THE BEST.');
   alert('MmHmm, MmHmm, very interesting.');
-} else  if (q3 == 'N'||q3=='NO') {
-      var a3 = 'No';
-      var a3Result = 'incorrectly';
-      console.log('Strike!');
-      alert('MmHmm, MmHmm, very interesting.');
-  }    else {
-        var a3 = q3;
-        var a3Result = 'something that made no sense.';
+  }
+  else  if (q3 == 'N'||q3=='NO') {
+    var a3 = 'No';
+    var a3Result = ' which was incorrect';
+    console.log('Strike!');
+    alert('MmHmm, MmHmm, very interesting.');
+    }
+    else {
+      var a3 = q3;
+      var a3Result =  ' which made no sense';
       };
 
 //declare and ask the fourth question.
@@ -84,17 +89,19 @@ var q4 = q4.toUpperCase();
 //determine whether the user answered yes or no.
 if(q4 == 'Y'||q4=='YES') {
   var a4 = 'Yes';
-  var a4Result = 'correctly';
+  var a4Result = ' which was correct';
   console.log('What\'s an Instructional Designer?');
   alert('MmHmm, MmHmm, very interesting.');
-} else  if (q4 == 'N'||q4=='NO') {
-      var a4 = 'No';
-      var a4Result = 'incorrectly';
-      console.log('Strike!');
-      alert('MmHmm, MmHmm, very interesting.');
-  }     else {
-        var a4 = q4;
-        var a4Result = 'something that made no sense.';
+  }
+  else  if (q4 == 'N'||q4=='NO') {
+    var a4 = 'No';
+    var a4Result = ' which was incorrect';
+    console.log('Strike!');
+    alert('MmHmm, MmHmm, very interesting.');
+    }
+    else {
+      var a4 = q4;
+      var a4Result = ' which made no sense';
       };
 
 //declare and ask the fifth question.
@@ -103,25 +110,31 @@ var q5 = q5.toUpperCase();
 //determine whether the user answered yes or no.
 if(q5 == 'Y'||q5=='YES') {
   var a5 = 'Yes';
-  var a5Result = 'correctly';
+  var a5Result = ' which was correct';
   console.log('HECK YES YOU DO, BECAUSE THEY\'RE FREAKING AWESOME!');
   alert('MmHmm, MmHmm, very interesting.');
-} else  if (q5 == 'N'||q5=='NO') {
-      var a5 = 'No';
-      var a5Result = 'incorrectly';
-      console.log('Strike!');
-      alert('MmHmm, MmHmm, very interesting.');
-  }     else {
-        var a5 = q5;
-        var a5Result = 'something that made no sense.';
+  }
+  else  if (q5 == 'N'||q5=='NO') {
+    var a5 = 'No';
+    var a5Result = ' which was incorrect';
+    console.log('Strike!');
+    alert('MmHmm, MmHmm, very interesting.');
+    }
+    else {
+      var a5 = q5;
+      var a5Result = ' which made no sense';
       };
 
 
 
 
 //Write the user's answers to the page as they answer
-document.querySelector('.answer1').innerHTML = '<p>The Quiz Master asked you whether Jed has ever eaten a live squid. You ' + a1Result + ' said ' +'\"'+a1+'\".'+'</p>'
-document.querySelector('.answer2').innerHTML = '<p>He then asked whether Jed has been to paris or not. You ' + a2Result + ' said ' +'\"'+a2+'\".'+'</p>'
-document.querySelector('.answer3').innerHTML = '<p>This next one was strange. How are you supposed to know whether or not Jed would choose to be a Polar Bear? You ' + a3Result + ' said ' +'\"'+a3+'\".'+'</p>'
-document.querySelector('.answer4').innerHTML = '<p>Next, he asked whether people knew generally knew what an Instructional Designer was. You ' + a4Result + ' said ' +'\"'+a4+'\".'+'</p>'
-document.querySelector('.answer5').innerHTML = '<p>Thank goodness this was the last question. That quizmaster has terrible breath. He brought up the Polar Bear again, asking whether or not you like them. You ' + a5Result + ' said ' +'\"'+a5+'\".'+'</p>'
+document.querySelector('.answer1').innerHTML = '<p>The Quiz Master asked you whether Jed has ever eaten a live squid. You said ' +'\"'+a1+',\"'+ a1Result +'.</p>'
+
+document.querySelector('.answer2').innerHTML = '<p>He then asked whether Jed has been to paris or not. You said ' +'\"'+a2+',\"'+ a2Result +'.</p>'
+
+document.querySelector('.answer3').innerHTML = '<p>This next one was strange. How are you supposed to know whether or not Jed would choose to be a Polar Bear? You said ' +'\"'+a3+',\"'+ a3Result +'.</p>'
+
+document.querySelector('.answer4').innerHTML = '<p>Next, he asked whether people knew generally knew what an Instructional Designer was. You said ' +'\"'+a4+',\"'+ a4Result +'.</p>'
+
+document.querySelector('.answer5').innerHTML = '<p>Thank goodness this was the last question. That quizmaster has terrible breath. He brought up the Polar Bear again, asking whether or not you like them. You said ' +'\"'+a5+',\"'+ a5Result +'.</p>'
