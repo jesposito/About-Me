4fe056eeec6c89293e7e9e7440aaec48f5bf684e
Jedidiah Esposito <egerthe@gmail.com>
2017-05-31 19:38:24 -0700

    declaring vars and test array

---
+++ quiz.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var position = 0;
+var correct = 0;
+var quizStatus, question;
+
+var questions = [
+['question 1 text', 'incorrect response 1', 'correct response 1'],
+['question 2 text', 'incorrect response 2', 'correct response 2'],
+['question 3 text', 'incorrect response 3', 'correct response 3'],
+['question 4 text', 'incorrect response 4', 'correct response 4'],
+['question 5 text', 'incorrect response 5', 'correct response 5']
+];
+
+console.log(questions[0][0]);
