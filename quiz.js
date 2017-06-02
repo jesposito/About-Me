a28cc13591e61d9e2cefed3f328f6b1d8e536e53
Jedidiah Esposito <egerthe@gmail.com>
2017-06-01 18:05:45 -0700

    worked out array in jsbin and added content. Switched from app.js to quiz.js

--- quiz.js
+++ quiz.js
@@ -1,24 +1,77 @@
 'use strict';
 
+var inQuote = ' \"';
+var outQuote = '\" ';
+var outQuote2 = '\"';
+var apostrophe = '\'';
 var position = 0;
+var totalQuestions = 7;
 var correct = 0;
-var quizStatus, question;
+var quizStatus = correct + ' correct out of ' + totalQuestions;
 
-//this array contains the question text and correct and incorrect feedback for each question
+//welcome the user and ask them if they're ready to take the quiz.
+var confirmed = confirm('Welcome! Before you go any further, I\'d like to find out how well you know me. Are you ready for a quiz?');
+//determine whether or not they're ready and respond.
+if(confirmed === true){
+  console.log('The user is ready!');
+  //let the user know we heard them.
+  alert('Great! I\'ll call the Quiz Master over.');
+} else {
+  //let the user know we can't stop it now.
+  console.log('The user said they were not ready... but we\'re gonna start anyway.');
+  alert('Ah nuts. I already sen\'t for the Quiz Master. He\'ll be here any minute. Be a pal and do it anyway, will ya?');
+}
+
+//ask the user for their name and store it.
+var name = prompt('Oh! I almost forgot. What should I call you?');
+
+//introduce the quiz maaster who will be asking the questions.
+alert('Quiz Master: You called? Who needs quizzing?');
+//razz the user a little for fun.
+alert('Quiz Master: Oh... it\'s this one. (sigh) alright then, ' + name + '. Let\'s get it over with.');
+
+//an array to store each of the first 5 questions and all of the associated brikabrak
 var questions = [
-['question 1 text', 'incorrect response 1', 'correct response 1', 'correct answer'],
-['question 2 text', 'incorrect response 2', 'correct response 2', 'correct answer'],
-['question 3 text', 'incorrect response 3', 'correct response 3', 'correct answer'],
-['question 4 text', 'incorrect response 4', 'correct response 4', 'correct answer'],
-['question 5 text', 'incorrect response 5', 'correct response 5', 'correct answer']
+  [],
+//Question text:[i][0]    Incorrect feedback:[i][1]    Correct feedback:[i][2]    Correct Answer(yes || no):[i][3]    The user's response:[i][4]
+['Has Jed ever eaten a live squid?', 'Wrong! No need to get into it here, but he did.', 'Good guess. He did indeed. Do not try it yourself if you can avoid it.', 'yes', 'user answer'],
+['Has Jed ever been to Paris?', 'Whoops. Not yet, but he plans to go soon!', 'Correct. He plans to go soon, though.', 'no', 'user answer'],
+['If Jed were an animal, would he be a Polar Bear?', 'WRONG! There is no better animal. Period.', 'HECK YES HE WOULD!', 'yes', 'user answer'],
+['When Jed shares his profession, do people usually look at him quizically?', 'Unfortunately, most people have no idea what an Instructional Designer is.', 'That\'s right. Nobody knows.', 'yes', 'user answer'],
+['Do you like Polar Bears, too?', 'What is wrong with you?', 'HECK YES YOU DO!', 'yes', 'user answer']
 ];
 
-//iterate through the questions array logging each question once
-for (i = 0; i < questions.length; i++) {
-  console.log(questions[i][0]);
+//iterate through the questions array, asking each question, recording the answer and providing feedback
+// noprotect
+for (i = 1; i <= questions.length; i++) {
+  //adjust the user's position in the quiz.
+  position++;
+  //ask the question and place the user's response into the appropriate place in the questions array
+  questions[i].splice(4,1,prompt(questions[i][0]));
+  //adjust the user's answer to account for edge cases
+  if(questions[i][4].toLowerCase() == 'y' || questions[i][4].toLowerCase() == 'yes'){
+    questions[i].splice(4,1,'yes');
+  } else if (questions[i][4].toLowerCase() == 'n' || questions[i][4].toLowerCase() == 'no') {
+    questions[i].splice(4,1,'no');
+  } else if (questions[i][4] === '') {
+    questions[i].splice(4,1,'a silent, steely-eyed stare');
+  } else {
+    questions[i].splice(4,1,'something nonsensical');
+  }
+  //log the user's answer to check if fixes were applied to edge cases.
+  console.log(questions[i][4]);
+  console.log('i is currently ' + i);
+  //check the user's answer against the answer in the array. If it matches do something. If not, do something else.
+  if(questions[i][4] == questions[i][3]) {
+    //add a point to the correct variable
+    correct++;
+    //write the user's answer to the page and let them know if it was right or wrong.
+    document.querySelector('.answer' + i).innerHTML = '<p>The Quiz Master asked, ' + inQuote + questions[i][0] + outQuote + 'You responded with' + inQuote + questions[i][4] + outQuote2 + '. ' + questions[i][2] + '</p>';
+  } else if (questions[i][4] == 'a silent, steely-eyed stare') {
+    document.querySelector('.answer' + i).innerHTML = '<p>The Quiz Master asked, ' + inQuote + questions[i][0] + outQuote + 'You responded with' + questions[i][4] + '. ' + questions[i][1] + '</p>';
+  } else {
+    document.querySelector('.answer' + i).innerHTML = '<p>The Quiz Master asked, ' + inQuote + questions[i][0] + outQuote + 'You responded with' + inQuote + questions[i][4] + outQuote2 + '. ' + questions[i][1] + '</p>';
+  }
 }
-
-//iterate through the questions array prompting the user for input with each question one time
-for (i = 0; i < questions.length; i++) {
-  var answer = prompt(questions[i][0]);
-}
+//log the current position to the console
+console.log(position);
